4738e3e91fa5a5e51620ab42775deacaff0c8e49
Matias <jackomaster303@gmail.com>
2021-10-07 20:03:23 -0300

    Agregamos el testswitch.js

--- src/components/test/Test.Switch.js
+++ src/components/test/Test.Switch.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
 
-import {ProjectGroupDataTable} from '../common/DataTable';
-import { ProjectGroupDatagrid } from '../common/DataGrid'
+import {
+    RFDialog
+} from './ForwardRef'
+
+import Form2 from './Form2'
+import Form5 from './Form5';
+
 
 export default function TestSwitch(props) {
 
     let { path, url } = useRouteMatch();
-
 
     return (
         <>
@@ -15,12 +19,22 @@ export default function TestSwitch(props) {
 
                 {/* Projecct switch pages */}
                 <Route path={`${path}/1`}>
-                    <ProjectGroupDataTable/>
+                    <h4>Test 1</h4>
+                </Route>
+                <Route path={`${path}/form2`}>
+                    <Form2 />
+                </Route>
+                <Route path={`${path}/form5`}>
+                    <Form5 />
                 </Route>
 
                 {/* user switch pages */}
                 <Route path={`${path}/2`}>
-                    <ProjectGroupDatagrid/>
+                    <h2>Test 2</h2>
+                    <br />
+                    <Link to={`${url}/form2`}>Al Form 2</Link>
+                    <br />
+                    <Link to={`${url}/form5`}>Al Form 5</Link>
                 </Route>
 
                 {/* app main pages */}
